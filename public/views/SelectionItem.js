54fab23ce0e3cceafcd9c66df2a8242f259c8ed0
Rob Middleton <rob@i.tv>
2012-03-26 17:24:35 -0600

    won state

--- public/views/SelectionItem.js
+++ public/views/SelectionItem.js
@@ -16,6 +16,7 @@ function (_
             this.$level = this.$el.find(".level")
             this.$link = this.$el.find(".link")
             this.$user = this.$el.find(".user")
+            this.$state = this.$el.find(".state")
             this.render()
         },
 
@@ -24,6 +25,9 @@ function (_
             this.$link.attr("href", "#/game/" + this.model.gameId)
             if (this.model.player && this.model.player.username) {
                 this.$user.text(this.model.player.username)
+            }
+            if (this.model.state.mode) {
+                this.$state.text(this.model.state.mode)
             }
         }
     })
