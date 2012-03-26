73065351cd19f79d5ede544294be73ab11e8e1f9
bryce <bryce@i.tv>
2012-03-26 17:16:46 -0600

    added an async library for great good

--- demos/java/Driver.java
+++ demos/java/Driver.java
@@ -1,7 +1,21 @@
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+
+
 class Driver {
+	public static void main (String [] args) throws Exception {
+		new AsyncHttpClient().prepareGet("http://www.google.com/").execute(new AsyncCompletionHandler<Response>(){
 
-	public static void main (String [] args) {
+		    @Override
+		    public Response onCompleted(Response response) throws Exception{
+		    	System.out.println(response.getResponseBody());
+		        return response;
+		    }
 
-
+		    @Override
+		    public void onThrowable(Throwable t){
+		    }
+		});
 	}
 }
