f7b4e91d850d4acab16560ef594c31d37fd7daa6
Rob Merrell <robmerrell@i.tv>
2012-03-26 17:20:33 -0600

    Added the ruby client

---
+++ demos/ruby/main.rb
@@ -0,0 +1,12 @@
+HOST = 'http://dev.i.tv:2663' # Set this to the address of the game server
+
+require "rubygems"
+require "bundler/setup"
+require "httparty"
+require "./robo_game"
+
+
+game = RoboGame.new("username", "my_email_address@i.tv", "github_repo_with_teh_codez")
+game.start_game :one # one, two, three, etc...
+game.move :down # can be right, left, down or up
+puts game.game_state.inspect
